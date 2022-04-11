30907480bf989c88d0062cb8294fb2e6bc5a62b4
Beniamin Guras <gurasbeniamin@gmail.com>
2022-04-11 16:35:05 +0200

    Add price change

--- src/js/script.js
+++ src/js/script.js
@@ -58,7 +58,11 @@
       thisProduct.id = id;
       thisProduct.data = data;
       thisProduct.renderInMenu();
+      thisProduct.getElements();
       thisProduct.initAccordion();
+      thisProduct.initOrderForm();
+      thisProduct.processOrder();
+
       console.log('this product', thisProduct);
     }
 
@@ -70,11 +74,20 @@
       menuContainer.appendChild(thisProduct.element);
     }
 
+    getElements(){
+      const thisProduct = this;
+    
+      thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
+      thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
+      thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
+      thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+    }
+
     initAccordion(){
       const thisProduct = this;
-      const clickableTriger = thisProduct.element.querySelector(select.menuProduct.clickable);
-      console.log('clickableTriger', clickableTriger);
-      clickableTriger.addEventListener('click', function(event){
+      
+      thisProduct.accordionTrigger.addEventListener('click', function(event){
         event.preventDefault;
         const activeProduct = document.querySelector('.product.active');
         console.log('activeProduct to: ', activeProduct);
@@ -84,7 +97,57 @@
         thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
       });
     }
+
+    initOrderForm(){
+      const thisProduct = this;
+      console.log('initOrderForm:');
+      thisProduct.form.addEventListener('submit', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+      
+      for(let input of thisProduct.formInputs){
+        input.addEventListener('change', function(){
+          thisProduct.processOrder();
+        });
+      }
+      
+      thisProduct.cartButton.addEventListener('click', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+    }
+
+    processOrder(){
+      const thisProduct = this;
+
+      const formData = utils.serializeFormToObject(thisProduct.form);
+      console.log('formData', formData);
+    
+      let price = thisProduct.data.price;
+    
+      for(let paramId in thisProduct.data.params) {
+        const param = thisProduct.data.params[paramId];
+        console.log('paramId', paramId, param);
+        
+        for(let optionId in param.options) {
+          const option = param.options[optionId];
+          console.log(optionId, option);
+          if(formData[paramId] && formData[paramId].includes(optionId)) {
+            if(!option.hasOwnProperty('default')) {
+              price = price + option.price;
+            }
+          } else {
+            if(option.hasOwnProperty('default')){
+              price = price - option.price;
+            }
+          }
+        }
+      }
+      thisProduct.priceElem.innerHTML = price;
+    }
   }
+
   const app = {
     initMenu: function(){
       const thisApp = this;
